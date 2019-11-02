c43a6f237a922bf4b6d605ab575ce4343c9f718d
aarniolaura <tania.aarnio@gmail.com>
2019-11-02 12:00:52 +0200

    added tokenization and created nltk text

--- week1_task.py
+++ week1_task.py
@@ -4,24 +4,26 @@
 # 2. Extract plain text from the HTML document
 # 3. Extract only certain parts of the text
 
-# From nltk book ch 3:
 import nltk, re, pprint
 from nltk import word_tokenize
 from urllib import request
 from bs4 import BeautifulSoup
 
+# From nltk book ch 3:
 
 # download the document
 url = "http://news.bbc.co.uk/2/hi/health/2284783.stm"
 html = request.urlopen(url).read().decode('utf8')
-print(html)
+#print(html)
 
 # Use beautiful soup to find the text in the file
 raw = BeautifulSoup(html, 'html.parser').get_text()
 tokens = word_tokenize(raw)
-tokens
+print(tokens)
 
 # create an NLTK text from this list of tokens
-
+text = nltk.Text(tokens)
+print(type(text))
+print(text)
 
 
