33712b3ab9eb18ba28ae9694cea56437f559c6a1
aarniolaura <tania.aarnio@gmail.com>
2019-11-06 11:22:05 +0200

    #5 removed image caption (at least on this day's text)

--- week1_task.py
+++ week1_task.py
@@ -5,7 +5,6 @@
 # 3. Extract only certain parts of the text
 
 import nltk, re, pprint
-from nltk import word_tokenize
 from urllib import request
 from bs4 import BeautifulSoup
 from datetime import date
@@ -13,37 +12,31 @@ from datetime import date
 today = date.today()
 day = today.strftime("%B %d, %Y")
 print("Today is ", day)
-
+print()
 print("Historical events that happened on this day according to Wikipedia:\n")
 
-# download the document
+# 1. Download the content
 url = "https://en.wikipedia.org/wiki/Main_Page"
 html = request.urlopen(url).read().decode('utf8')
 #print(html)
 
-# Use beautiful soup to find the text in the file
+# Remove this thumbcaption now because it's easier to find in html:
+# <div class="thumbcaption" style="padding: 0.25em 0; word-wrap: break-word;">George Eliot</div></div>
+html = re.sub(r'<div class="thumbcaption"', '', html)
+html = re.sub(r'style="padding:.*word-wrap:', '', html)
+html = re.sub(r'break-word;">.*</div></div>', '', html)
+
+# 2. Use beautiful soup to extract the text in the html file
 raw = BeautifulSoup(html, 'html.parser').get_text()
 
+# 3. Extract the wanted segment:
 # starting point of the text segment
 onthisday = raw.find("On this day")
 # ending point of the text segment
 anniv = raw.find("More anniversaries:")
 
-# Extract the wanted segment:
 raw = raw[onthisday:anniv]
 
 # Remove excess new lines
 segment = re.sub('\n\n\n', '\n', raw)
 print(segment)
-
-
-# tokenization (if needed)
-
-# tokens = word_tokenize(raw)
-# print(tokens)
-
-# create an NLTK text from this list of tokens (if needed)
-
-# text = nltk.Text(tokens)
-# print(type(text))
-# print(text)
