3745e7e392b1b0f303e0654266fe25adb57a970e
Teemu Pöyhönen <teme.polhonen@gmail.com>
2019-11-07 13:37:19 +0200

    #6
    Program shows the five first articles (alphabetically) and the amount of matched articles.

--- search_engine.py
+++ search_engine.py
@@ -16,10 +16,10 @@ except OSError:
 # Split into lists of strings (each article is a string)
 documents = wikipedia.split('</article>')
 
-print(len(documents))
+#print(len(documents))
 
-for i in documents:
-    print(i)
+#for i in documents:
+#    print(i)
 
 # Create a dictionary (article name: article contents) if needed
 dict = {}
@@ -35,14 +35,14 @@ cv = CountVectorizer(lowercase=True, binary=True)
 # print a sparse matrix
 sparse_matrix = cv.fit_transform(documents)
 
-print("Document-term matrix: (?)\n")
-print(sparse_matrix)
+#print("Document-term matrix: (?)\n")
+#print(sparse_matrix)
 
 # Anyway, let's print a _dense_ version of this matrix:
 dense_matrix = sparse_matrix.todense()
 
-print("Document-term matrix: (?)\n")
-print(dense_matrix)
+#print("Document-term matrix: (?)\n")
+#print(dense_matrix)
 
 td_matrix = dense_matrix.T   # .T transposes the matrix
 terms = cv.get_feature_names()
@@ -67,14 +67,27 @@ def test_query(query):
     print("Matching:", eval(rewrite_query(query))) # Eval runs the string as a Python command
     print()
 
-test_query("example AND NOT nothing")
+def show_doc(query):
+    hits_matrix = eval(rewrite_query(query))
+    hits_list = list(hits_matrix.nonzero()[1])
+#   print(hits_list)
+    count = 0
+    for doc_idx in hits_list:
+        print("Matching doc:", documents[doc_idx][:200] + "...")
+        print()
+        count += 1
+        if count > 4:
+            return print("Showing the first five of",len(hits_list),"articles.")
 
 
 # Ask the user to type a query
-query = str(input("Enter a query (enter blank if you want to quit): "))
+query = str(input("Enter a query: "))
 while query != "":
     # hits_matrix = eval(rewrite_query(query))
     test_query(query)
+    show_doc(query)
+
+
     query = str(input("Enter a new query (enter blank if you want to quit): "))
 else:
     print("Goodbye!")
