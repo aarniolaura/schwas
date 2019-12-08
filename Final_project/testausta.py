874e2bf266989db18aab1e96f92ccbabc069d3dc
aarniolaura <tania.aarnio@gmail.com>
2019-12-08 17:31:41 +0200

    #18 meaning file fixed

--- Final_project/testausta.py
+++ Final_project/testausta.py
@@ -1,17 +1,24 @@
-import spacy
-from spacy import displacy
-from pathlib import Path
+import re
 
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("This is a sentence.")
+text_doc = ""
+file_name = "meanings_en.txt"
+try:
+    file = open(file_name, "r", encoding='utf-8')
+    text_doc = file.read()
+    file.close()
+except OSError:
+    print("Error reading the file", file_name)
 
-options = {"color": "#D01271", "bg": "#B5FFF6"}
-svg = displacy.render(doc, style="dep", options=options, jupyter=False)
+list = text_doc.split('\n<ENDS HERE>')
 
-file_name = '-'.join([w.text for w in doc if not w.is_punct]) + ".svg"
-print(file_name)
-output_path = Path("static/" + file_name)
-output_path.open("w", encoding="utf-8").write(svg)
 
-# python -m spacy download es_core_news_sm
-# nlp = spacy.load("es_core_news_sm")
+filename = "edit_mean.txt"
+
+with open(filename, "w", encoding="utf-8") as file:  # change 'w' to 'a' to append
+    for item in list:
+        pois = item.find("\n")
+        item = item[pois+1:]
+        item = re.sub("\[edit\]", ": ", item)
+        file.write(item + "\n<ENDS HERE>")
+
+file.close()
